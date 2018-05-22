21dfe5fbf8826825aaabe0c17fea8a9bba5c8fea
Max0705 <569736866@qq.com>
2018-05-22 17:30:02 +0800

    Initial commit

---
+++ src/Test/test.java
@@ -0,0 +1,10 @@
+package Test;
+
+import javax.jms.Connection;
+import org.apache.activemq.ActiveMQConnection;
+
+public class test {
+    public static void main(String[] args){
+        System.out.print("hello donghaosheng!");
+    }
+}
