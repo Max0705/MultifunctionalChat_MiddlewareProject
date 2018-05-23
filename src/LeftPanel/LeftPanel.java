2e20eb9deffda93c0906d8edd03f60e2c0e3f0f5
chy <1260147774@qq.com>
2018-05-23 21:41:45 +0800

    2018.5.22 from cuiheyu

--- src/LeftPanel/LeftPanel.java
+++ src/LeftPanel/LeftPanel.java
@@ -5,14 +5,11 @@ import Tools.GBC;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by song on 17-5-29.
- */
 public class LeftPanel extends JPanel
 {
     //private MyInfoPanel myInfoPanel;
-    //private SearchPanel searchPanel;
-    //private TabOperationPanel mainOperationPanel;
+    private SearchPanel searchPanel;
+    private TabOperationPanel mainOperationPanel;
     private ListPanel listPanel;
 
     public LeftPanel()
@@ -28,7 +25,7 @@ public class LeftPanel extends JPanel
 
         //searchPanel = new SearchPanel(this);
 
-        //mainOperationPanel = new TabOperationPanel(this);
+        mainOperationPanel = new TabOperationPanel(this);
         //mainOperationPanel.setBackground(Color.blue);
 
         listPanel = new ListPanel(this);
@@ -42,7 +39,7 @@ public class LeftPanel extends JPanel
 
         //add(myInfoPanel, new GBC(0, 0).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(1, 7));
         //add(searchPanel, new GBC(0, 1).setAnchor(GBC.CENTER).setFill(GBC.HORIZONTAL).setWeight(1, 1));
-        //add(mainOperationPanel, new GBC(0, 2).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(1, 1));
+        add(mainOperationPanel, new GBC(0, 2).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(1, 1));
         add(listPanel, new GBC(0, 3).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(1, 60));
 
     }
