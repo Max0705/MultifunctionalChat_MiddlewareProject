58a1e3d2f892b3bf0894dd9fde76fcb44a57e29b
Max0705 <569736866@qq.com>
2018-05-25 09:54:31 +0800

    rightpanel of mainframe

---
+++ src/RightPanel/BaseMessageViewHolder.java
@@ -0,0 +1,12 @@
+package RightPanel;
+
+import javax.swing.*;
+
+/**
+ * Created by song on 13/06/2017.
+ */
+public class BaseMessageViewHolder extends JPanel
+{
+    public JLabel avatar = new JLabel();
+    public JLabel time = new JLabel();
+}
